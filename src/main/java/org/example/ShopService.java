3ccf2c128bac4320fe80d45ac7e6b2f0031e7368
neuefische <info@neuefische.de>
2023-09-12 17:29:24 +0200

    add addOrder method

--- src/main/java/org/example/ShopService.java
+++ src/main/java/org/example/ShopService.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class ShopService {
 
@@ -29,6 +32,26 @@ public class ShopService {
             orderRepo.addOrder(order, id);
             orderMapRepo.addOrder(order, id);
         }
+    }
+
+    public Order addOrder(List<String> productIds) {
+        List<Product> products = new ArrayList<>();
+        try {
+            for (String productId : productIds) {
+                Optional<Product> productToOrder = productRepo.getProductById(productId);
+                if (productToOrder.isEmpty()) {
+                    //System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+                    throw new ProductNotFoundException(productId);
+                }
+                products.add(productToOrder.get());
+            }
+        } catch (ProductNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Order newOrder = new Order(new Product("Laptop", "2"), "1", OrderStatus.PROCESSING);
+
+        return orderRepo.addOrder(newOrder, "1");
     }
 
     public List<Order> orderStatus(OrderStatus status) {
